4e92cb5fc49ba18d4ef5278853687bfe63886a90
James Jesudason <james.jesudason@gmail.com>
2020-04-07 09:18:39 +0100

    Add updated JS files

---
+++ static/precache-manifest.7866d7d5c82f1886c4f6a435be6e5c3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57d69bb973fcf25d8d71868efecf709a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5db4c0f6ab26fa9f1f2c",
+    "url": "/static/css/main.c66acf6f.chunk.css"
+  },
+  {
+    "revision": "771df87990b2e09b099e",
+    "url": "/static/js/2.4dccf779.chunk.js"
+  },
+  {
+    "revision": "13ce2a23dade87340be10f0141a46470",
+    "url": "/static/js/2.4dccf779.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5db4c0f6ab26fa9f1f2c",
+    "url": "/static/js/main.dfbc73d0.chunk.js"
+  },
+  {
+    "revision": "ed6df68fcee1b439126d",
+    "url": "/static/js/runtime-main.f13e748c.js"
+  }
+]);
